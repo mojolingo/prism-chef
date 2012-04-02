4dc64886ac081bbf899ef4114f40e0a1bf7c4cf2
John Dyer <johntdyer@gmail.com>
2012-04-02 19:57:42 -0400

    bumped to version 11.5.0.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "jdyer@voxeo.com"
 license          "All rights reserved"
 description      "Installs/Configures Prism application server http://voxeo.com/prism"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "11.5.0"
+version          "11.5.0.0"
 
 depends "nokogiri"
 depends "jmxsh"
